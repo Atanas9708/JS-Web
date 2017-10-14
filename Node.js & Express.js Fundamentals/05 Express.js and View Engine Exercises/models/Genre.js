a1906e7408a0d607ac2b264c8a37f86922030585
Atanas9708 <nasko_9708@mail.bg>
2017-10-14 18:02:57 +0300

    .

--- Node.js & Express.js Fundamentals/05 Express.js and View Engine Exercises/models/Genre.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-let genreSchema = mongoose.Schema({
-    genreName: { type: mongoose.Schema.Types.String, required: true, unique: true},
-    memeList: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Meme' }]
-});
-
-module.exports = mongoose.model('Genre', genreSchema);

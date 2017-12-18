1f7474ec25eaffbce3f5dbe24d3d287e1c35574d
Atanas9708 <nasko_9708@mail.bg>
2017-12-18 16:47:31 +0200

    .

--- Angular Fundamentals/asd/client/src/app/components/shared/index.ts
+++
@@ -1,7 +0,0 @@
-import { SearchTableComponent } from './search-table/search-table.component';
-import { NotFoundComponent } from './not-found/not-found.component';
-
-export const sharedComponents = [
-    SearchTableComponent,
-    NotFoundComponent
-];

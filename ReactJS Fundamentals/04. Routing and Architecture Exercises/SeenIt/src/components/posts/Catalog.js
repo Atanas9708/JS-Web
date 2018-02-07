7275a7c67dddca56dae852433a27743b2dc0aa7e
Atanas9708 <nasko_9708@mail.bg>
2018-02-07 21:39:51 +0200

    Fixed issue with reloading page on redirect

--- ReactJS Fundamentals/04. Routing and Architecture Exercises/SeenIt/src/components/posts/Catalog.js
+++ ReactJS Fundamentals/04. Routing and Architecture Exercises/SeenIt/src/components/posts/Catalog.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import Post from './Post';
+import { Link } from 'react-router-dom';
 import reqHandler from './../../utils/reqHandler';
+import notify from './../../notifications/notify';
 
 class Catalog extends Component {
     constructor(props) {
@@ -27,7 +28,49 @@ class Catalog extends Component {
         return (
             <div className="posts">
                 {this.state.posts.map((p, i) => {
-                    return <Post key={i} data={p} rank={rank++} />
+                    return <article key={i} className="post">
+                        <div className="col rank">
+                            <span>{rank++}</span>
+                        </div>
+                        <div className="col thumbnail">
+                            <a href={p.url}>
+                                <img src={p.imageUrl} />
+                            </a>
+                        </div>
+                        <div className="post-content">
+                            <div className="title">
+                                <a href={p.url}>
+                                    {p.title}
+                                </a>
+                            </div>
+                            <div className="details">
+                                <div className="info">
+                                    submitted {reqHandler.calcTime(p._kmd.ect)} ago by {p.author}
+                                </div>
+                                <div className="controls">
+                                    <ul>
+                                        {p.author === localStorage.getItem('username') &&
+                                            <div>
+                                                <li className="action"><Link className="commentsLink" to={`/details/${p._id}`}>details</Link></li>
+                                                <li className="action"><Link className="editLink" to={`/editPost/${p._id}`}>edit</Link></li>
+                                                <li className="action"><a onClick={() => {
+                                                    reqHandler.deletePost(p._id)
+                                                        .then(() => {
+                                                            let newPosts = this.state.posts.filter(x => x._id !== p._id);
+                                                            this.setState({ posts: newPosts });
+                                                            notify.showError('Post deleted!');
+                                                        });
+                                                }} className="deleteLink" href="javascript:void(0)">delete</a></li>
+                                            </div>
+                                        }
+                                        {p.author !== localStorage.getItem('username') &&
+                                            <li className="action"><Link className="commentsLink" to={`/details/${p._id}`}>details</Link></li>
+                                        }
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </article>
                 })}
             </div>
         )
--- ReactJS Fundamentals/04. Routing and Architecture Exercises/SeenIt/src/components/posts/Details.js
+++ ReactJS Fundamentals/04. Routing and Architecture Exercises/SeenIt/src/components/posts/Details.js
@@ -1,35 +1,30 @@
 import React, { Component } from 'react';
 import reqHandler from './../../utils/reqHandler';
 import { Link } from 'react-router-dom';
-
-import Comment from './../comments/Comment';
-import CreateComment from './../comments/CreateComment';
+import notify from './../../notifications/notify';
 
 class Details extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
+            comment: '',
             comments: [],
             post: {},
             time: ''
         }
     }
 
-    loadComments = (postId) => {
-        reqHandler.loadCommentsById(postId)
-            .then(comments => {
-                this.setState({ comments: comments });
-            })
+    onChange = (e) => {
+        this.setState({ [e.target.name]: e.target.value });
     }
-
 
     componentDidMount() {
         const postId = this.props.match.params.postId;
         reqHandler.loadPostById(postId)
             .then(post => {
                 let time = reqHandler.calcTime(post._kmd.ect);
-                this.setState({ post, time: time })
+                this.setState({ post, time })
             })
             .then(() => {
                 this.loadComments(postId);
@@ -37,9 +32,32 @@ class Details extends Component {
     }
 
 
-    componentDidUpdate() {
-        const postId = this.props.match.params.postId;
-        this.loadComments(postId);
+    loadComments = (postId) => {
+        reqHandler.loadCommentsById(postId)
+            .then(comments => {
+                this.setState({ comments });
+            })
+    }
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        if (this.state.comment === null) {
+            notify.showError('The content of the comment cannot be empty!');
+            return;
+        }
+        let data = {
+            postId: this.props.match.params.postId,
+            content: this.state.comment,
+            author: localStorage.getItem('username')
+        };
+        reqHandler.createComment(data)
+            .then(response => {
+                notify.showInfo('Comment created!');
+                let newComments = this.state.comments.slice();
+                newComments.unshift(response);
+                this.setState({ content: '', comments: newComments });
+                document.getElementById('input').value = '';
+            })
     }
 
     render() {
@@ -77,10 +95,30 @@ class Details extends Component {
                     </div>
                     <div className="clear"></div>
                 </div>
-                <CreateComment id={this.props.match.params.postId} />
+                <div className="post post-content">
+                    <form id="commentForm" onSubmit={this.onSubmit}>
+                        <label>Comment</label>
+                        <textarea onChange={this.onChange} id="input" name="comment" type="text"></textarea>
+                        <input type="submit" value="Add Comment" id="btnPostComment" />
+                    </form>
+                </div>
                 {this.state.comments.map((c, i) => {
-                    return <Comment key={i} data={c} />
+                    return <article key={i} className="post post-content">
+                        <p>{c.content}</p>
+                        <div className="info">
+                            submitted {reqHandler.calcTime(c._kmd.ect)} ago by {c.author} |
+            {localStorage.getItem('username') === c.author ? <a onClick={() => {
+                                reqHandler.deleteComment(c._id)
+                                    .then(() => {
+                                        let newComments = this.state.comments.filter(x => x._id !== c._id);
+                                        this.setState({ comments: newComments });
+                                        notify.showInfo('Comment deleted!');
+                                    })
+                            }} href="javascript:void(0)" className="deleteLink">Delete</a> : null}
+                        </div>
+                    </article>
                 })}
+
             </section>
         )
     }
